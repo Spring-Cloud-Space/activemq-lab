6dbc29eedf2abc31a51ee71cb6343d9e2f73c9a0
yul <yu.li@tecsys.com>
2020-07-03 07:44:02 -0400

    Initial Commit

---
+++ src/main/java/yulikexuan/cloud/jms/activemqlab/ActivemqLabApplication.java
@@ -0,0 +1,18 @@
+//: yulikexuan.cloud.jms.activemqlab.ActivemqLabApplication.java
+
+
+package yulikexuan.cloud.jms.activemqlab;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class ActivemqLabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ActivemqLabApplication.class, args);
+    }
+
+}
---
+++ src/test/java/yulikexuan/cloud/jms/activemqlab/ActivemqLabApplicationIT.java
@@ -0,0 +1,18 @@
+//: yulikexuan.cloud.jms.activemqlab.ActivemqLabApplicationIT.java
+
+
+package yulikexuan.cloud.jms.activemqlab;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+class ActivemqLabApplicationIT {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
