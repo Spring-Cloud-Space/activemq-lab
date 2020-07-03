8409ab9ddb0fcffb447b0b0b90baed16caa5970f
yul <yu.li@tecsys.com>
2020-07-03 09:40:38 -0400

    Configuring an embedded ActiveMQ Artemis server. Closes #1

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Apache ActiveMQ Lab
+
+### API
+- [Artemis API](https://activemq.apache.org/components/artemis/documentation/javadocs/javadoc-latest/)
+- [artemis.core.config](https://access.redhat.com/webassets/avalon/d/red-hat-jboss-enterprise-application-platform/7.0.0/javadocs/org/apache/activemq/artemis/core/config/package-summary.html)
+- [artemis.core.server](https://access.redhat.com/webassets/avalon/d/red-hat-jboss-enterprise-application-platform/7.0.0/javadocs/org/apache/activemq/artemis/core/server/package-summary.html)
---
+++ src/main/java/yulikexuan/cloud/jms/activemqlab/domain/model/HelloWorldMessage.java
@@ -0,0 +1,26 @@
+//: yulikexuan.cloud.jms.activemqlab.domain.model.HelloWorldMessage.java
+
+
+package yulikexuan.cloud.jms.activemqlab.domain.model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+
+@Data
+@NoArgsConstructor
+@Builder @AllArgsConstructor
+public class HelloWorldMessage implements Serializable {
+
+    static final long serialVersionUID = -6703826490277916847L;
+
+    private UUID id;
+    private String message;
+
+}///:~
